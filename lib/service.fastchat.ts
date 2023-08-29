65039ad8ee88c6a9a3c2baa351551d095567a29a
Nathan Stitt <nathan@stitt.org>
2023-08-29 17:57:10 -0500

    switch to streaming

--- lib/service.fastchat.ts
+++ lib/service.fastchat.ts
@@ -1,33 +1,26 @@
 import { SavedMessageModel, SavedChatModel, Message, messagesForChatId } from './data'
 import { RequestContext } from './request-context'
 import { getParamStoreValue } from './aws'
-import fetch from 'node-fetch'
-
+import { fetchEventSource } from 'fetch-event-source-hperrin'
 
 import { IS_PROD } from './env'
 
+class CompletedError extends Error {  }
+const MAX_ATTEMPTS = 3
 
 type Chunk = {
     id: string
     model: string
     choices: Array<{
         index: number
-        message: {
+        delta: {
             role: string
-            content: string
+            content?: string
         }
     }>
     finish_reason: null | string
 }
 
-type TutorBotReply = {
-    "Thoughts of Tutorbot": string
-    "Evaluation of Student Response": string
-    "Action Based on Evaluation": string
-    "Subproblem State": string
-    "Subproblem": string
-    "Tutorbot": string
-}
 
 export const requestInference = async (
     chat: SavedChatModel, message: SavedMessageModel, ctx: RequestContext,
@@ -42,9 +35,16 @@ export const requestInference = async (
     if (IS_PROD && !token) {
         token = await getParamStoreValue('together-ai-api-token')
     }
-    if (!token) throw new Error("No token for together.ai")
+    if (!token) throw new Error("No token for fastchat/together.ai")
 
-    const response = await fetch('https://luffy-chat.staging.kinetic.openstax.org/v1/chat/completions', {
+    const saveAndStream = (msg: SavedMessageModel, isPending = true) => {
+        Message.update(msg)
+        ctx.onProgress({ msgId: message.id, content: message.content, isPending })
+    }
+
+    let attempts = 0
+
+    await fetchEventSource('https://luffy-chat.staging.kinetic.openstax.org/v1/chat/completions', {
         method: 'POST',
         headers: {
             Authorization: `Bearer ${token}`,
@@ -59,26 +59,66 @@ export const requestInference = async (
             top_p: 0.7,
             top_k: 50,
             repetition_penalty: 1,
-            //stream: true,
-        })
+            stream: true,
+        }),
+        onmessage(chunk) {
+            console.log(chat.id, chunk.data)
+            if (chunk.data == '[DONE]') {
+                message.content = message.content.trim()
+                saveAndStream(message)
+                ctx.onComplete()
+                controller.abort()
+                throw new CompletedError()
+            }
+            const msg = JSON.parse(chunk.data) as Chunk
+            const content = msg.choices[0].delta.content
+
+            if (!content) return
+
+            message.content += message.content.length ? content : content.trimStart()
+
+            message.content = message.content
+                .replace(/^(\r\n|\r|\n)*<?TutorBot>?:(\r\n|\r|\n)*/gi, '')
+                .replace(/(\r\n|\r|\n){2,}/g, '\n\n')
+
+            saveAndStream(message)
+        },
+        onerror(err) {
+            if (attempts > MAX_ATTEMPTS || err instanceof CompletedError) {
+                throw err // do not retry
+            } else {
+                attempts += 1
+            }
+        },
+        onclose() {
+            if (message.content.length) { // we've got at least some content
+                ctx.onComplete()
+            }
+        }
+    }).catch(err => {
+        if (err instanceof CompletedError) {
+            return
+        } else {
+            console.warn("ERROR", err)
+        }
     })
 
-    console.log(response.ok)
+    // console.log(response.ok)
 
-    const json = await response.json() as Chunk
-    let msg: string = json.choices[0].message.content
-    try {
-         msg = (JSON.parse(json.choices[0]?.message?.content) as TutorBotReply).Tutorbot
-    } catch(e) {
-        console.log(`CAUGHT, msg was:\n${json.choices[0]?.message?.content}`)
-        console.warn(e)
-    }
+    // const json = await response.json() as Chunk
+    // let msg: string = json.choices[0].message.content
+    // try {
+    //      msg = (JSON.parse(json.choices[0]?.message?.content) as TutorBotReply).Tutorbot
+    // } catch(e) {
+    //     console.log(`CAUGHT, msg was:\n${json.choices[0]?.message?.content}`)
+    //     console.warn(e)
+    // }
 
-    console.log(JSON.stringify(json))
+    // console.log(JSON.stringify(json))
 
-    message.content = msg
+    // message.content = msg
 
-    Message.update(message)
-    ctx.onProgress({ msgId: message.id, content: message.content, isPending: false })
-    ctx.onComplete()
+    // Message.update(message)
+    // ctx.onProgress({ msgId: message.id, content: message.content, isPending: false })
+    // ctx.onComplete()
 }
