4ed75152e0b0bb910a4a4f3e78bb504cd24150a7
Nathan Stitt <nathan@stitt.org>
2023-09-13 12:16:24 -0500

    support old model key "quiz", stale browsers

--- server/service.ts
+++ server/service.ts
@@ -4,9 +4,7 @@ import type { ChatUpdatesQueue } from './chat-updates'
 import { RequestContext } from './request-context'
 
 export const requestInference = async (chat: SavedChatModel, message: SavedMessageModel, ctx: RequestContext) => {
-    console.log({ model: ctx.model })
-
-    if (ctx.model == 'self-hosted') {
+    if (ctx.model == 'self-hosted' || ctx.model == 'quiz') {
         const { requestInference } = await import('./service.fastchat.js')
         return requestInference(chat, message, ctx)
     }
