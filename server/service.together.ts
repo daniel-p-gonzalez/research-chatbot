e59b3044e1a4b357954fc8cf2bacffdda4fe97cc
Nathan Stitt <nathan@stitt.org>
2023-09-21 09:23:12 -0500

    cleanup inference services

--- server/service.together.ts
+++ server/service.together.ts
@@ -30,7 +30,6 @@ function messageForPrompt<M extends InferenceMessage>(m: M) {
 export async function requestInference(ctx: InferenceContext) {
     const { buildPrompt, cleanMessageContent, PROMPT_INST_SUFFIX } = await import('./prompts')
 
-
     const prompt = buildPrompt(ctx, ctx.transcript.length == 0) + ctx.transcript.map(messageForPrompt).join('\n\n') + PROMPT_INST_SUFFIX
 
     const token = await getConfigValue('together-ai-api-token')
@@ -102,8 +101,10 @@ export const inferenceForChat = async (
     chat: SavedChatModel, message: SavedMessageModel, ctx: RequestContext,
 ) => {
 
+    const msgs = await messagesForChatId(chat.id)
+
     // the last two messages will be the one we're
-    const transcript = (await messagesForChatId(chat.id)).slice(0, -2)
+    const transcript = msgs.slice(0, -2)
 
     return requestInference({
         ...ctx,
@@ -119,84 +120,5 @@ export const inferenceForChat = async (
             ctx.onProgress({ msgId: message.id, content, isPending: true })
         }
     })
-
-    // const controller = new AbortController()
-    // const messages = await messagesForChatId(chat.id)
-    // let attempts = 0
-
-    // const { buildPrompt, cleanMessageContent, PROMPT_INST_SUFFIX } = await import('./prompts')
-
-    // const prompt = buildPrompt(ctx, messages) + messages.slice(0, -2).map(messageForPrompt).join('\n\n') + PROMPT_INST_SUFFIX
-
-    // console.log(ctx, prompt)
-
-    // const token = await getConfigValue('together-ai-api-token')
-
-    // const saveAndStream = (msg: SavedMessageModel, isPending = true) => {
-    //     Message.update(msg)
-    //     ctx.onProgress({ msgId: message.id, content: message.content, isPending })
-    // }
-    // const response = fetchEventSource('https://api.together.xyz/api/inference', {
-    //     method: 'POST',
-    //     headers: {
-    //         Authorization: `Bearer ${token}`,
-    //         'Content-Type': 'application/json',
-    //     },
-    //     signal: controller.signal,
-    //     body: JSON.stringify({
-    //         prompt: ctx.message,
-    //         model: ctx.model,
-    //         prompt_format_string: prompt,
-    //         max_tokens: 768,
-    //         temperature: 0.7,
-    //         top_p: 0.7,
-    //         type: 'chat',
-    //         stop: ['</s>:', '[INST'],
-    //         top_k: 50,
-    //         repetition_penalty: 1,
-    //         stream_tokens: true,
-    //     }),
-    //     onmessage(chunk) {
-    //         console.log(chat.id, chunk.data)
-    //         if (chunk.data == '[DONE]') {
-    //             message.content = message.content.trim()
-    //             saveAndStream(message)
-    //             ctx.onComplete()
-    //             controller.abort()
-    //             throw new CompletedError()
-    //         }
-    //         const msg = JSON.parse(chunk.data) as Chunk
-    //         const content = msg.choices[0].text
-
-    //         if (msg.token.special) { // FIXME: determine what "special" means.  have emailed support
-    //             message.content = content
-    //         } else {
-    //             message.content += message.content.length ? content : content.trimStart()
-    //         }
-    //         message.content = cleanMessageContent(message.content)
-
-    //         saveAndStream(message)
-    //     },
-    //     onerror(err) {
-    //         if (attempts > MAX_ATTEMPTS || err instanceof CompletedError) {
-    //             throw err // do not retry
-    //         } else {
-    //             attempts += 1
-    //         }
-    //     },
-    //     onclose() {
-    //         if (message.content.length) { // we've got at least some content
-    //             ctx.onComplete()
-    //         }
-    //     }
-    // }).catch(err => {
-    //     if (err instanceof CompletedError) {
-    //         return
-    //     } else {
-    //         console.warn("ERROR", err)
-    //     }
-    // })
-
-    // return response
 
 }
